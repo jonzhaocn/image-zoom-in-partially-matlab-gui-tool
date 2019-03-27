4d95b5640c17c531b5368d70b31cfaa0c8ef5483
JonZhao <1044264932@qq.com>
2019-03-27 19:19:49 +0800

    avoid moving areas out of the image

--- move_rect.m
+++ move_rect.m
@@ -4,10 +4,28 @@ function move_rect(row, col, handles, config)
     row_move = row - info.rect_move_start_point(1);
     col_move = col - info.rect_move_start_point(2);
     
+    if info.rect_position(1) + col_move < 1
+        col_move = 1 - info.rect_position(1);
+        col = col_move + info.rect_move_start_point(2);
+    end
+    if info.rect_position(1) + col_move + info.rect_position(3) > size(config.image, 2)
+        col_move = size(config.image, 2) - info.rect_position(1) - info.rect_position(3);
+        col = col_move + info.rect_move_start_point(2);
+    end
+    
+    if info.rect_position(2) + row_move < 1
+        row_move = 1 - info.rect_position(2);
+        row = row_move + info.rect_move_start_point(1);
+    end
+    if info.rect_position(2) + row_move + info.rect_position(4) > size(config.image, 1)
+        row_move = size(config.image, 1) - info.rect_position(2) - info.rect_position(4);
+        row = row_move + info.rect_move_start_point(1);
+    end
+    
     info.rect_move_start_point(1) = row;
     info.rect_move_start_point(2) = col;
+    delete(findobj(gca, 'tag', config.rect_tag));
     
-    delete( findobj(gca, 'tag', config.rect_tag) );
     info.rect_position(1) = info.rect_position(1) + col_move;
     info.rect_position(2) = info.rect_position(2) + row_move;
     rectangle(handles.rect_plot_handle, 'position', info.rect_position, 'edgecolor', config.edgecolor, 'tag', config.rect_tag);
--- save_images.m
+++ save_images.m
@@ -29,8 +29,8 @@ function save_images(config, images_list)
             imwrite(zoomed_img, zoomed_img_path);
             fprintf('save image:%s\n', zoomed_img_path);
             % -------------------------------- marked image
-            img(row_s:row_e, [col_s, col_e], :) = repmat(color_value, num_row, 2, 1);
-            img([row_s,row_e], col_s:col_e, :) = repmat(color_value, 2, num_col, 1);
+            img(row_s:row_e, [col_s, col_e], :) = repmat(color_value, row_e-row_s+1, 2, 1);
+            img([row_s,row_e], col_s:col_e, :) = repmat(color_value, 2, col_e-col_s+1, 1);
             marked_img_path = fullfile(config.zoomed_image_save_dir, sprintf('./%s_marked.png', img_name));
             imwrite(img, marked_img_path);
             fprintf('save image:%s\n', marked_img_path);
